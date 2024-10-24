4c2c5c4c390a6cc332d48e6abdb1fa3b97a4fc4b
varunbasavarajprathmesh <prathmesh.dollin@klejtbcagadag.com>
2024-10-24 13:18:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("body").ripples({
+resolution: 512,
+dropRadius: 20,
+perturbance: 0.04,
+});
+
+  $(".poster").ripples({
+resolution: 512,
+dropRadius: 20,
+perturbance: 0.04,
+});
+});
